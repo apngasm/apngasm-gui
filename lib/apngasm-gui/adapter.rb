a08df7416e94d51dbe20010593f5ee9c2163167b
Rika Yoshida <nazuki@genshin.org>
2014-03-05 15:37:31 +0900

    import apng file

--- lib/apngasm-gui/adapter.rb
+++ lib/apngasm-gui/adapter.rb
@@ -1,8 +1,8 @@
-# require 'rapngasm'
+require 'rapngasm'
 require 'fileutils'
 require_relative 'frame_list.rb'
 require_relative 'frame.rb'
-require_relative 'rapngasm.bundle'
+# require_relative 'rapngasm.bundle'
 
 class APNGAsmGUI::Adapter
   def initialize
@@ -45,12 +45,19 @@ class APNGAsmGUI::Adapter
   end
 
   def set_apngframe(frame)
-    # TODO
-    frame.apngframe.delay_numerator(frame.delay)
-    frame.apngframe
-    # new_frame = APNGFrame.new(frame.filename)
-    # new_frame.delay_numerator(frame.delay)
-    # new_frame
+    # frame.apngframe.delay_numerator(frame.delay)
+    # frame.apngframe
+    generator = APNGFrameGenerator.new
+    new_frame = APNGFrame.new
+    filename = "#{File.basename(frame.filename, '.png')}"
+
+    Dir::mktmpdir(nil, File.dirname(__FILE__)) do |dir|
+      frame.apngframe.save("#{dir}/#{filename}")
+      new_frame = generator.init_with_file("#{dir}/#{filename}")
+    end
+
+    new_frame.delay_numerator(frame.delay)
+    new_frame
   end
 
   def set_filename(filename)
